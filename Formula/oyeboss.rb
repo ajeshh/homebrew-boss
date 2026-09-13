52156f207939097afff34640489307fb7d34c8f2
Aj S <9387065+ajeshh@users.noreply.github.com>
2026-09-13 10:57:34 -0700

    boss 0.325.0

--- Formula/oyeboss.rb
+++ Formula/oyeboss.rb
@@ -1,8 +1,8 @@
 class Oyeboss < Formula
   desc "Just-in-time startup incubator that mentors founders from idea to venture"
   homepage "https://oyeboss.build"
-  url "https://registry.npmjs.org/oyeboss/-/oyeboss-0.306.0.tgz"
-  sha256 "dbebd2335ca1843c1f732a6ac6b912d7a355802176ccc7cbbfc4c9d405ffc04c"
+  url "https://registry.npmjs.org/oyeboss/-/oyeboss-0.325.0.tgz"
+  sha256 "aaa12821d385db0f6e9c96722d8a3c6df78f0166181ea611b95987b3b6b3c4e7"
   license "MIT"
 
   depends_on "node"
@@ -13,6 +13,6 @@ class Oyeboss < Formula
   end
 
   test do
-    assert_match "0.306.0", shell_output("#{bin}/boss --version")
+    assert_match "0.325.0", shell_output("#{bin}/boss --version")
   end
 end
